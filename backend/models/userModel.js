cebf3421a9a69a62d0f870829a33e0fb23d58350
iririb <eshamibrahim94@gmail.com>
2022-10-11 01:09:07 +0800

    Implementing token for login process

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -23,10 +23,10 @@ userSchema.statics.signup = async function (email, password) {
     if (!email || !password) {
         throw Error('All fields must be filled')
     }
-    if(!validator.isEmail(email)){
+    if (!validator.isEmail(email)) {
         throw Error('Email is not valid')
     }
-    if(!validator.isStrongPassword(password)){
+    if (!validator.isStrongPassword(password)) {
         throw Error('Password not strong enough')
     }
 
@@ -40,6 +40,24 @@ userSchema.statics.signup = async function (email, password) {
     const hash = await bcrypt.hash(password, salt)
 
     const user = await this.create({ email, password: hash })
+
+    return user
+}
+
+// static login method
+userSchema.statics.login = async function (email, password) {
+    // validation
+    if (!email || !password) {
+        throw Error('All fields must be filled')
+    }
+    const user = await this.findOne({ email })
+    if (!user) {
+        throw Error('Incorrect email')
+    }
+    const match = await bcrypt.compare(password, user.password)
+    if (!match) {
+        throw Error('Incorrect password')
+    }
 
     return user
 }
