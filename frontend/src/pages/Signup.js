fb2b3d2d3dee9de0fa1f96785378464eeb03c016
iririb <eshamibrahim94@gmail.com>
2022-10-12 23:35:14 +0800

    Creating sigup hook

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,15 +1,18 @@
 import { useState } from 'react'
+import { useSignup } from '../hooks/useSignup'
 
 const Signup = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const { signup, isLoading, error } = useSignup()
 
-    const handleSubmit = async (e) =>{
+    const handleSubmit = async (e) => {
         e.preventDefault()
-        console.log(email, password)
+        await signup(email, password)
+
     }
     return (
-        <form className="signup" onSubmit={handleSubmit }>
+        <form className="signup" onSubmit={handleSubmit}>
             <h3>Sign up</h3>
             <label>Email</label>
             <input
@@ -23,7 +26,8 @@ const Signup = () => {
                 onChange={(e) => setPassword(e.target.value)}
                 value={password}
             />
-            <button>Sign up</button>
+            <button disabled={isLoading}>Sign up</button>
+            {error && <div className="error">{error}</div>}
         </form>
     )
 }
