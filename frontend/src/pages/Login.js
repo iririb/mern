028b3750f7816b4541149230a1cd9b996b3a7005
iririb <eshamibrahim94@gmail.com>
2022-10-12 23:55:47 +0800

    Creating login logic

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,15 +1,18 @@
 import { useState } from 'react'
+import { useLogin } from '../hooks/useLogin'
 
 const Login = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const { login, error, isLoading } = useLogin()
 
-    const handleSubmit = async (e) =>{
+    const handleSubmit = async (e) => {
         e.preventDefault()
-        console.log(email, password)
+
+        await login(email, password)
     }
     return (
-        <form className="login" onSubmit={handleSubmit }>
+        <form className="login" onSubmit={handleSubmit}>
             <h3>Log in</h3>
             <label>Email</label>
             <input
@@ -23,7 +26,8 @@ const Login = () => {
                 onChange={(e) => setPassword(e.target.value)}
                 value={password}
             />
-            <button>Log in</button>
+            <button disabled={isLoading}>Log in</button>
+            {error && <div className="error">{error}</div>}
         </form>
     )
 }
